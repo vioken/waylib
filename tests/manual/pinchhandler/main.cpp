230490671c1e4fd89d1a61bbcc25f5f8ce655c83
rewine <luhongxu@deepin.org>
2023-07-28 18:20:40 +0800

    Support touch event
    close: https://github.com/vioken/waylib/issues/99

---
+++ tests/manual/pinchhandler/main.cpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_QPA_PLATFORM", "wayland");
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/pinchhandler/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+        &app, []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
