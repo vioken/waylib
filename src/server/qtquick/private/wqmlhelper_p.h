4cf62d4fe0c70efecb43e1e5d1f93e68b3497969
JiDe Zhang <zhangjide@deepin.org>
2023-08-18 20:07:45 +0800

    Support rotation and scale output on software renderer

--- src/server/qtquick/private/wqmlhelper_p.h
+++ src/server/qtquick/private/wqmlhelper_p.h
@@ -5,12 +5,53 @@
 
 #include <wglobal.h>
 #include <QQmlEngine>
+#define protected public
+#include <QImage>
+#undef protected
 
 QT_BEGIN_NAMESPACE
 class QQuickItem;
+class QPainter;
 QT_END_NAMESPACE
 
 WAYLIB_SERVER_BEGIN_NAMESPACE
+
+class WImageRenderTarget : public QPaintDevice
+{
+public:
+    explicit WImageRenderTarget();
+
+    int devType() const override;
+    QPaintEngine *paintEngine() const override;
+
+    inline WImageRenderTarget &operator =(const QImage &other) {
+        *image.get() = other;
+        return *this;
+    }
+    inline operator const QImage&() const {
+        return *image.get();
+    }
+
+    inline const uchar *constBits() const {
+        return image->constBits();
+    }
+
+    inline bool isNull() const {
+        return image->isNull();
+    }
+
+    inline QSize size() const {
+        return image->size();
+    }
+
+    void setDevicePixelRatio(qreal dpr);
+
+private:
+    int metric(PaintDeviceMetric metric) const override;
+    QPaintDevice *redirected(QPoint *offset) const override;
+
+    mutable std::unique_ptr<QImage> image;
+};
 
 class WAYLIB_SERVER_EXPORT WQmlHelper : public QObject
 {
