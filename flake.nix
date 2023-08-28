27cad82c4e6748d44a10f87cd2c36198035ad407
rewine <luhongxu@deepin.org>
2023-08-28 15:28:28 +0800

    Remove QT_NO_DEBUG from qt-base-hook

--- flake.nix
+++ flake.nix
@@ -44,11 +44,14 @@
               echo "welcome to waylib"
               echo "wlroots: $(pkg-config --modversion wlroots)"
               echo "wayland-server: $(pkg-config --modversion wayland-server)"
+              # unexpected QT_NO_DEBUG form qt-base-hook
+              # https://github.com/NixOS/nixpkgs/issues/251918
+              export NIX_CFLAGS_COMPILE=$(echo $NIX_CFLAGS_COMPILE | sed 's/-DQT_NO_DEBUG//')
               export QT_LOGGING_RULES="*.debug=true;qt.*.debug=false"
-              export MESA_DEBUG=1
-              export EGL_LOG_LEVEL=debug
-              export LIBGL_DEBUG=verbose
-              export WAYLAND_DEBUG=1
+              #export MESA_DEBUG=1
+              #export EGL_LOG_LEVEL=debug
+              #export LIBGL_DEBUG=verbose
+              #export WAYLAND_DEBUG=1
               export QT_PLUGIN_PATH=${makeQtpluginPath (with pkgs.qt6; [ qtbase qtdeclarative qtquick3d qtwayland ])}
               export QML2_IMPORT_PATH=${makeQmlpluginPath (with pkgs.qt6; [ qtdeclarative qtquick3d ])}
               export QML_IMPORT_PATH=$QML2_IMPORT_PATH
