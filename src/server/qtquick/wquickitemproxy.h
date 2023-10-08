f9b2adb6e0b0295243e61704a6675220b738ecfd
JiDe Zhang <zhangjide@deepin.org>
2023-10-08 18:05:35 +0800

    Add WQuickItemProxy

---
+++ src/server/qtquick/wquickitemproxy.h
@@ -0,0 +1,58 @@
+// Copyright (C) 2023 JiDe Zhang <zhangjide@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <wglobal.h>
+#include <QQuickItem>
+
+WAYLIB_SERVER_BEGIN_NAMESPACE
+
+class WQuickItemProxyPrivate;
+class WAYLIB_SERVER_EXPORT WQuickItemProxy : public QQuickItem, public WObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QQuickItem* sourceItem READ sourceItem WRITE setSourceItem NOTIFY sourceItemChanged)
+    Q_PROPERTY(QRectF sourceRect READ sourceRect WRITE setSourceRect NOTIFY sourceRectChanged)
+    Q_PROPERTY(qreal implicitWidth READ implicitWidth NOTIFY implicitWidthChanged FINAL)
+    Q_PROPERTY(qreal implicitHeight READ implicitHeight NOTIFY implicitHeightChanged FINAL)
+    Q_PROPERTY(bool hideSource READ hideSource WRITE setHideSource NOTIFY hideSourceChanged)
+    Q_PROPERTY(bool mipmap READ mipmap WRITE setMipmap NOTIFY mipmapChanged)
+    W_DECLARE_PRIVATE(WQuickItemProxy)
+    QML_NAMED_ELEMENT(ItemProxy)
+
+public:
+    explicit WQuickItemProxy(QQuickItem *parent = nullptr);
+    ~WQuickItemProxy() override;
+
+    QQuickItem* sourceItem() const;
+    void setSourceItem(QQuickItem* sourceItem);
+
+    QRectF sourceRect() const;
+    void setSourceRect(const QRectF &sourceRect);
+
+    bool hideSource() const;
+    void setHideSource(bool newHideSource);
+
+    bool isTextureProvider() const override;
+    QSGTextureProvider *textureProvider() const override;
+
+    bool mipmap() const;
+    void setMipmap(bool newMipmap);
+
+Q_SIGNALS:
+    void sourceItemChanged();
+    void sourceRectChanged();
+    void fixedChanged();
+    void hideSourceChanged();
+    void mipmapChanged();
+
+protected:
+    QSGNode *updatePaintNode(QSGNode *old, UpdatePaintNodeData *) override;
+    void componentComplete() override;
+    void itemChange(ItemChange change, const ItemChangeData &data) override;
+
+    W_PRIVATE_SLOT(void onTextureChanged())
+};
+
+WAYLIB_SERVER_END_NAMESPACE
