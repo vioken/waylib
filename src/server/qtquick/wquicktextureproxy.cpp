4a11b79ee62ce2e4c69fdd4f7a92bbde6f90779c
Yixue Wang <wangyixue@deepin.org>
2023-11-08 16:18:46 +0800

    chore: correct license for wquicktextureproxy.cpp
    Correct license identifier for wquicktextureproxy.cpp.
    
    Log: correct license identifier for wquicktextureproxy.cpp

--- src/server/qtquick/wquicktextureproxy.cpp
+++ src/server/qtquick/wquicktextureproxy.cpp
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2020 - 2022 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "wquicktextureproxy.h"
 #include "wquicktextureproxy_p.h"
