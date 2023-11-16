8d51e585c6d5f859226c5bece4f8361fa8bec3b1
justforlxz <justforlxz@gmail.com>
2023-11-16 20:01:16 +0800

    Implement WQuickXdgDecorationManagerAttached

--- src/server/qtquick/private/wquickxdgdecorationmanager_p.h
+++ src/server/qtquick/private/wquickxdgdecorationmanager_p.h
@@ -10,13 +10,34 @@
 
 WAYLIB_SERVER_BEGIN_NAMESPACE
 
+class WSurface;
+class WQuickXdgDecorationManager;
 class WQuickXdgDecorationManagerPrivate;
+class WAYLIB_SERVER_EXPORT WQuickXdgDecorationManagerAttached : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool serverDecorationEnabled READ serverDecorationEnabled NOTIFY serverDecorationEnabledChanged FINAL)
+    QML_ANONYMOUS
+
+public:
+    WQuickXdgDecorationManagerAttached(WSurface *target, WQuickXdgDecorationManager *manager);
+
+    bool serverDecorationEnabled() const;
+
+Q_SIGNALS:
+    void serverDecorationEnabledChanged();
+
+private:
+    WSurface *m_target;
+    WQuickXdgDecorationManager *m_manager;
+};
 class WAYLIB_SERVER_EXPORT WQuickXdgDecorationManager : public WQuickWaylandServerInterface, public WObject
 {
     Q_OBJECT
     W_DECLARE_PRIVATE(WQuickXdgDecorationManager)
     Q_PROPERTY(DecorationMode mode READ mode WRITE setMode NOTIFY modeChanged)
     Q_PROPERTY(WSocket* ownsSocket READ ownsSocket CONSTANT)
+    QML_ATTACHED(WQuickXdgDecorationManagerAttached)
     QML_NAMED_ELEMENT(XdgDecorationManager)
 
 public:
@@ -30,10 +51,16 @@ public:
     Q_ENUM(DecorationMode)
 
     void setMode(DecorationMode mode);
+    void setModeBySurface(WSurface *surface, DecorationMode mode);
+
     DecorationMode mode() const;
+    DecorationMode modeBySurface(WSurface* surface) const;
+
+    static WQuickXdgDecorationManagerAttached *qmlAttachedProperties(QObject *target);
 
 Q_SIGNALS:
     void modeChanged(DecorationMode mode);
+    void surfaceModeChanged(WSurface *surface, DecorationMode mode);
 
 private:
     void create() override;
