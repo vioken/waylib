5b609e68cd6e4bca79afd7c97938d61fe61c676b
rubbishzhyic <zhongyicheng@uniontech.com>
2024-05-23 15:18:43 +0800

    fix: sync tinywl-qtquick to use client-driven resize
    complement of https://github.com/vioken/waylib/pull/308

--- examples/tinywl/main.cpp
+++ examples/tinywl/main.cpp
@@ -406,7 +406,7 @@ bool Helper::beforeDisposeEvent(WSeat *seat, QWindow *watched, QInputEvent *even
 
                 if (surface->checkNewSize(geo.size().toSize())) {
                     surfaceItem->setPosition(geo.topLeft());
-                    surfaceItem->setSize(geo.size());
+                    surfaceItem->resizeSurface(geo.size().toSize());
                 }
             }
 
