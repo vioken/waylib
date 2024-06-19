06aaad1f4809dc9c13c366c2e0d1f50fe256679c
rewine <luhongxu@deepin.org>
2024-06-19 10:20:37 +0800

    Add missing pragma once and move func to cpp

--- src/server/qtquick/private/wsurfaceitem_p.h
+++ src/server/qtquick/private/wsurfaceitem_p.h
@@ -1,6 +1,8 @@
 // Copyright (C) 2023 JiDe Zhang <zhangjide@deepin.org>.
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
+#pragma once
+
 #include "wsurfaceitem.h"
 #include "wsurface.h"
 
