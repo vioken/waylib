b86a422c4db0f00f370e7273f27d4d4e72fba8f8
JiDe Zhang <zhangjide@deepin.org>
2024-08-02 18:22:58 +0800

    Fix surface's contents is not update when its opacity=0
    Maybe the SurfaceContentItem is used to MultiEffect::source, and
    use `opacity: 0` to hide the source item, but it wants also update
    the surface's contents in this case.

--- src/server/qtquick/wsurfaceitem.cpp
+++ src/server/qtquick/wsurfaceitem.cpp
@@ -210,7 +210,7 @@ public:
 
         // wayland protocol job should not run in rendering thread, so set context qobject to contentItem
         frameDoneConnection = QObject::connect(q->window(), &QQuickWindow::afterRendering, q, [this, q](){
-            if (q->rendered && live) {
+            if ((q->rendered || q->isVisible()) && live) {
                 surface->notifyFrameDone();
                 q->rendered = false;
             }
