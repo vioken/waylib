42c6af1c20ca531ce655b7b13f0daa555bed18ca
asterwyx <yixue.wang@outlook.com>
2024-09-06 13:19:32 +0800

    Remove Qt version check in WThreadUtils
    The project has been required Qt 6.5.

--- src/server/utils/wthreadutils.h
+++ src/server/utils/wthreadutils.h
@@ -140,10 +140,8 @@ private:
             static_assert(QtPrivate::CheckCompatibleArguments<QtPrivate::List<Args...>, typename FunInfo::Arguments>::value,
                           "The args and function are not compatible.");
         } else {
-#if QT_VERSION_MAJOR >= 6 && QT_VERSION_MIJOR > 5
             using Prototype = ReturnType(*)(Args...);
             QtPrivate::AssertCompatibleFunctions<Prototype, Func>();
-#endif
         }
 
         QPromise<ReturnType> promise;
