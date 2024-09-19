0196b4b7e3ba447f6f720f98052c975bfdb4d19b
rewine <luhongxu@deepin.org>
2024-09-19 16:38:40 +0800

    Support cursor alternative names form kwin

--- src/server/utils/wcursorimage.cpp
+++ src/server/utils/wcursorimage.cpp
@@ -18,6 +18,297 @@ Q_LOGGING_CATEGORY(qLcCursorImage, "waylib.server.cursor.image", QtWarningMsg)
 QW_USE_NAMESPACE
 WAYLIB_SERVER_BEGIN_NAMESPACE
 
+// This function copy form kwin 6.1.19 `src/cursor.cpp`
+// Copyright (C) 2013 Martin Gräßlin <mgraesslin@kde.org> GPL-2.0-or-later
+static QList<QByteArray> alternativesCursorShape(const QByteArray &name)
+{
+    static const QHash<QByteArray, QList<QByteArray>> alternatives = {
+        {
+            QByteArrayLiteral("crosshair"),
+            {
+                QByteArrayLiteral("cross"),
+                QByteArrayLiteral("diamond-cross"),
+                QByteArrayLiteral("cross-reverse"),
+            },
+        },
+        {
+            QByteArrayLiteral("default"),
+            {
+                QByteArrayLiteral("left_ptr"),
+                QByteArrayLiteral("arrow"),
+                QByteArrayLiteral("dnd-none"),
+                QByteArrayLiteral("op_left_arrow"),
+            },
+        },
+        {
+            QByteArrayLiteral("up-arrow"),
+            {
+                QByteArrayLiteral("up_arrow"),
+                QByteArrayLiteral("sb_up_arrow"),
+                QByteArrayLiteral("center_ptr"),
+                QByteArrayLiteral("centre_ptr"),
+            },
+        },
+        {
+            QByteArrayLiteral("wait"),
+            {
+                QByteArrayLiteral("watch"),
+                QByteArrayLiteral("progress"),
+            },
+        },
+        {
+            QByteArrayLiteral("text"),
+            {
+                QByteArrayLiteral("ibeam"),
+                QByteArrayLiteral("xterm"),
+            },
+        },
+        {
+            QByteArrayLiteral("all-scroll"),
+            {
+                QByteArrayLiteral("size_all"),
+                QByteArrayLiteral("fleur"),
+            },
+        },
+        {
+            QByteArrayLiteral("pointer"),
+            {
+                QByteArrayLiteral("pointing_hand"),
+                QByteArrayLiteral("hand2"),
+                QByteArrayLiteral("hand"),
+                QByteArrayLiteral("hand1"),
+                QByteArrayLiteral("e29285e634086352946a0e7090d73106"),
+                QByteArrayLiteral("9d800788f1b08800ae810202380a0822"),
+            },
+        },
+        {
+            QByteArrayLiteral("ns-resize"),
+            {
+                QByteArrayLiteral("size_ver"),
+                QByteArrayLiteral("00008160000006810000408080010102"),
+                QByteArrayLiteral("sb_v_double_arrow"),
+                QByteArrayLiteral("v_double_arrow"),
+                QByteArrayLiteral("n-resize"),
+                QByteArrayLiteral("s-resize"),
+                QByteArrayLiteral("col-resize"),
+                QByteArrayLiteral("top_side"),
+                QByteArrayLiteral("bottom_side"),
+                QByteArrayLiteral("base_arrow_up"),
+                QByteArrayLiteral("base_arrow_down"),
+                QByteArrayLiteral("based_arrow_down"),
+                QByteArrayLiteral("based_arrow_up"),
+            },
+        },
+        {
+            QByteArrayLiteral("ew-resize"),
+            {
+                QByteArrayLiteral("size_hor"),
+                QByteArrayLiteral("028006030e0e7ebffc7f7070c0600140"),
+                QByteArrayLiteral("sb_h_double_arrow"),
+                QByteArrayLiteral("h_double_arrow"),
+                QByteArrayLiteral("e-resize"),
+                QByteArrayLiteral("w-resize"),
+                QByteArrayLiteral("row-resize"),
+                QByteArrayLiteral("right_side"),
+                QByteArrayLiteral("left_side"),
+            },
+        },
+        {
+            QByteArrayLiteral("nesw-resize"),
+            {
+                QByteArrayLiteral("size_bdiag"),
+                QByteArrayLiteral("fcf1c3c7cd4491d801f1e1c78f100000"),
+                QByteArrayLiteral("fd_double_arrow"),
+                QByteArrayLiteral("bottom_left_corner"),
+                QByteArrayLiteral("top_right_corner"),
+            },
+        },
+        {
+            QByteArrayLiteral("nwse-resize"),
+            {
+                QByteArrayLiteral("size_fdiag"),
+                QByteArrayLiteral("c7088f0f3e6c8088236ef8e1e3e70000"),
+                QByteArrayLiteral("bd_double_arrow"),
+                QByteArrayLiteral("bottom_right_corner"),
+                QByteArrayLiteral("top_left_corner"),
+            },
+        },
+        {
+            QByteArrayLiteral("help"),
+            {
+                QByteArrayLiteral("whats_this"),
+                QByteArrayLiteral("d9ce0ab605698f320427677b458ad60b"),
+                QByteArrayLiteral("left_ptr_help"),
+                QByteArrayLiteral("question_arrow"),
+                QByteArrayLiteral("dnd-ask"),
+                QByteArrayLiteral("5c6cd98b3f3ebcb1f9c7f1c204630408"),
+            },
+        },
+        {
+            QByteArrayLiteral("col-resize"),
+            {
+                QByteArrayLiteral("split_h"),
+                QByteArrayLiteral("14fef782d02440884392942c11205230"),
+                QByteArrayLiteral("size_hor"),
+            },
+        },
+        {
+            QByteArrayLiteral("row-resize"),
+            {
+                QByteArrayLiteral("split_v"),
+                QByteArrayLiteral("2870a09082c103050810ffdffffe0204"),
+                QByteArrayLiteral("size_ver"),
+            },
+        },
+        {
+            QByteArrayLiteral("not-allowed"),
+            {
+                QByteArrayLiteral("forbidden"),
+                QByteArrayLiteral("03b6e0fcb3499374a867c041f52298f0"),
+                QByteArrayLiteral("circle"),
+                QByteArrayLiteral("dnd-no-drop"),
+            },
+        },
+        {
+            QByteArrayLiteral("progress"),
+            {
+                QByteArrayLiteral("left_ptr_watch"),
+                QByteArrayLiteral("3ecb610c1bf2410f44200f48c40d3599"),
+                QByteArrayLiteral("00000000000000020006000e7e9ffc3f"),
+                QByteArrayLiteral("08e8e1c95fe2fc01f976f1e063a24ccd"),
+            },
+        },
+        {
+            QByteArrayLiteral("grab"),
+            {
+                QByteArrayLiteral("openhand"),
+                QByteArrayLiteral("9141b49c8149039304290b508d208c40"),
+                QByteArrayLiteral("all_scroll"),
+                QByteArrayLiteral("all-scroll"),
+            },
+        },
+        {
+            QByteArrayLiteral("grabbing"),
+            {
+                QByteArrayLiteral("closedhand"),
+                QByteArrayLiteral("05e88622050804100c20044008402080"),
+                QByteArrayLiteral("4498f0e0c1937ffe01fd06f973665830"),
+                QByteArrayLiteral("9081237383d90e509aa00f00170e968f"),
+                QByteArrayLiteral("fcf21c00b30f7e3f83fe0dfd12e71cff"),
+            },
+        },
+        {
+            QByteArrayLiteral("alias"),
+            {
+                QByteArrayLiteral("link"),
+                QByteArrayLiteral("dnd-link"),
+                QByteArrayLiteral("3085a0e285430894940527032f8b26df"),
+                QByteArrayLiteral("640fb0e74195791501fd1ed57b41487f"),
+                QByteArrayLiteral("a2a266d0498c3104214a47bd64ab0fc8"),
+            },
+        },
+        {
+            QByteArrayLiteral("copy"),
+            {
+                QByteArrayLiteral("dnd-copy"),
+                QByteArrayLiteral("1081e37283d90000800003c07f3ef6bf"),
+                QByteArrayLiteral("6407b0e94181790501fd1e167b474872"),
+                QByteArrayLiteral("b66166c04f8c3109214a4fbd64a50fc8"),
+            },
+        },
+        {
+            QByteArrayLiteral("move"),
+            {
+                QByteArrayLiteral("dnd-move"),
+            },
+        },
+        {
+            QByteArrayLiteral("sw-resize"),
+            {
+                QByteArrayLiteral("size_bdiag"),
+                QByteArrayLiteral("fcf1c3c7cd4491d801f1e1c78f100000"),
+                QByteArrayLiteral("fd_double_arrow"),
+                QByteArrayLiteral("bottom_left_corner"),
+            },
+        },
+        {
+            QByteArrayLiteral("se-resize"),
+            {
+                QByteArrayLiteral("size_fdiag"),
+                QByteArrayLiteral("c7088f0f3e6c8088236ef8e1e3e70000"),
+                QByteArrayLiteral("bd_double_arrow"),
+                QByteArrayLiteral("bottom_right_corner"),
+            },
+        },
+        {
+            QByteArrayLiteral("ne-resize"),
+            {
+                QByteArrayLiteral("size_bdiag"),
+                QByteArrayLiteral("fcf1c3c7cd4491d801f1e1c78f100000"),
+                QByteArrayLiteral("fd_double_arrow"),
+                QByteArrayLiteral("top_right_corner"),
+            },
+        },
+        {
+            QByteArrayLiteral("nw-resize"),
+            {
+                QByteArrayLiteral("size_fdiag"),
+                QByteArrayLiteral("c7088f0f3e6c8088236ef8e1e3e70000"),
+                QByteArrayLiteral("bd_double_arrow"),
+                QByteArrayLiteral("top_left_corner"),
+            },
+        },
+        {
+            QByteArrayLiteral("n-resize"),
+            {
+                QByteArrayLiteral("size_ver"),
+                QByteArrayLiteral("00008160000006810000408080010102"),
+                QByteArrayLiteral("sb_v_double_arrow"),
+                QByteArrayLiteral("v_double_arrow"),
+                QByteArrayLiteral("col-resize"),
+                QByteArrayLiteral("top_side"),
+            },
+        },
+        {
+            QByteArrayLiteral("e-resize"),
+            {
+                QByteArrayLiteral("size_hor"),
+                QByteArrayLiteral("028006030e0e7ebffc7f7070c0600140"),
+                QByteArrayLiteral("sb_h_double_arrow"),
+                QByteArrayLiteral("h_double_arrow"),
+                QByteArrayLiteral("row-resize"),
+                QByteArrayLiteral("left_side"),
+            },
+        },
+        {
+            QByteArrayLiteral("s-resize"),
+            {
+                QByteArrayLiteral("size_ver"),
+                QByteArrayLiteral("00008160000006810000408080010102"),
+                QByteArrayLiteral("sb_v_double_arrow"),
+                QByteArrayLiteral("v_double_arrow"),
+                QByteArrayLiteral("col-resize"),
+                QByteArrayLiteral("bottom_side"),
+            },
+        },
+        {
+            QByteArrayLiteral("w-resize"),
+            {
+                QByteArrayLiteral("size_hor"),
+                QByteArrayLiteral("028006030e0e7ebffc7f7070c0600140"),
+                QByteArrayLiteral("sb_h_double_arrow"),
+                QByteArrayLiteral("h_double_arrow"),
+                QByteArrayLiteral("right_side"),
+            },
+        },
+    };
+    auto it = alternatives.find(name);
+    if (it != alternatives.end()) {
+        return it.value();
+    }
+    return QList<QByteArray>();
+}
+
 static inline wlr_xcursor *getXCursorWithFallback(qw_xcursor_manager *xcursor_manager, const char *name, float scale)
 {
     if (!xcursor_manager)
@@ -27,37 +318,11 @@ static inline wlr_xcursor *getXCursorWithFallback(qw_xcursor_manager *xcursor_ma
         return cursor;
     }
 
-    static const std::vector<std::vector<const char*>> typeLists = {
-        {"ibeam", "text", "xterm"},
-        {"openhand", "grab"},
-        {"crosshair", "cross", "all-scroll"},
-        {"closedhand", "dnd-move", "move", "dnd-none", "grabbing"},
-        {"dnd-copy", "copy"},
-        {"dnd-link", "link"},
-        {"row-resize", "size_ver", "ns-resize", "split_v", "n-resize", "s-resize"},
-        {"col-resize", "size_hor", "ew-resize", "split_h", "e-resize", "w-resize"},
-        {"nwse-resize", "nw-resize", "se-resize", "size_fdiag"},
-        {"progress", "wait", "watch"},
-        {"hand1", "hand2", "pointer", "pointing_hand"},
-    };
+    const auto alterNameList = alternativesCursorShape(name);
 
-    for (const auto &typeList : typeLists) {
-        bool hasType = std::find_if(typeList.begin(), typeList.end(), [name](const char *type) {
-                           return std::strcmp(type, name) == 0;
-                       }) != typeList.end();
-
-        if (!hasType)
-            continue;
-
-        for (const auto &type : typeList) {
-            if (std::strcmp(type, name) == 0)
-                continue;
-
-            if (auto cursor = xcursor_manager->get_xcursor(type, scale))
-                return cursor;
-        }
-
-        return nullptr;
+    for (const auto &alterName : alterNameList) {
+        if (auto cursor = xcursor_manager->get_xcursor(alterName, scale))
+            return cursor;
     }
 
     return nullptr;
